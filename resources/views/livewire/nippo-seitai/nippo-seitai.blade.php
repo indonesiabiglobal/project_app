62dbd3fbd069397ece3b54e2dbe14743c442d8bf
resna <resnapangestu01@gmail.com>
2024-06-21 02:51:56 +0700

    -

--- resources/views/livewire/nippo-seitai/nippo-seitai.blade.php
+++ resources/views/livewire/nippo-seitai/nippo-seitai.blade.php
@@ -1,4 +1,4 @@
-<title>Nippo Seitai</title>
+{{-- <title>Nippo Seitai</title> --}}
 <div class="container mt-5">
     <div class="row">
         @if (session()->has('message'))
@@ -8,39 +8,24 @@
         @endif
         <div class="col-lg-6 mb-4">
             <div class="form-group">
-                <label class="control-label col-md-3 col-xs-4" resources="DatePeriod"><span class="hidden-xs" style="white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">Filter </span>Tanggal</label>
+                <label class="control-label col-md-3 col-xs-4">
+                    <span class="hidden-xs" style="white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">Filter </span>Tanggal
+                </label>
                 <div class="input-group col-md-9 col-xs-8">
                     <table>
                         <tr style="white-space:nowrap">
                             <td class="hidden-xs" valign="top">
-                                <select class="form-select mb-0" id="gender"
-                                    aria-label="Gender select example">
-                                    <option selected>Proses</option>
-                                    <option value="Female">Order</option>
+                                <select class="form-select mb-0" wire:model.defer="transaksi">
+                                    <option value="1">Proses</option>
+                                    <option value="2">Order</option>
                                 </select>
                             </td>
                             <td>
                                 <div class="form-group" style="margin-left:1px; white-space:nowrap">
                                     <div class="input-group">
-                                        <span class="input-group-text"><svg class="icon icon-xs" fill="currentColor"
-                                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                                clip-rule="evenodd"></path>
-                                        </svg></span>
-                                    <input data-datepicker=""
-                                        class="form-control datepicker-input" id="birthday" type="text"
-                                        placeholder="yyyy/mm/dd">
+                                        <input class="form-control datepicker-input" type="date" wire:model.defer="tglMasuk" placeholder="yyyy/mm/dd"/>
 
-                                        <span class="input-group-text"><svg class="icon icon-xs" fill="currentColor"
-                                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                                clip-rule="evenodd"></path>
-                                        </svg></span>
-                                    <input data-datepicker=""
-                                        class="form-control datepicker-input" id="birthday" type="text"
-                                        placeholder="yyyy/mm/dd">
+                                        <input class="form-control datepicker-input" type="date" wire:model.defer="tglKeluar" placeholder="yyyy/mm/dd"/>
                                     </div>
                                 </div>
                             </td>
@@ -48,37 +33,42 @@
                     </table>
                 </div>
             </div>
-            <div class="form-group">
+            {{-- <div class="form-group">
                 <label class="control-label col-md-3 col-xs-4" resources="Search">Nomor LPK </label>
                 <div class="input-group col-md-9 col-xs-8">
                     <input id='searchText' name='searchText' class="form-control" type="text" resources-placeholder="SearchTextOrCode" placeholder="search nomor PO, nama produk" />
                 </div>
-            </div>
+            </div> --}}
             <div class="form-group">
                 <label class="control-label col-md-3 col-xs-4" resources="Search">Search</label>
                 <div class="input-group col-md-9 col-xs-8">
-                    <input id='searchText' name='searchText' class="form-control" type="text" resources-placeholder="SearchTextOrCode" placeholder="search nomor PO, nama produk" />
+                    <input id='searchText' name='searchText' class="form-control" type="text"  placeholder="search nomor PO, nama produk" />
                 </div>
             </div>
         </div>
     
         <div class="col-lg-6">
-            <div class="form-group">
+            {{-- <div class="form-group">
                 <label class="control-label col-md-3 col-xs-4" resources="OrgBranch">Produk</label>
                 <div class="input-group col-md-9 col-xs-8">
                     <select id='searchProd' name="searchProd" class="js-states form-control" placeholder="- all -"></select>
                 </div>
-            </div>
+            </div> --}}
             <div class="form-group">
-                <label class="control-label col-md-3 col-xs-4" resources="OrgDivision">Mesin</label>
+                <label class="control-label col-md-3 col-xs-4">Mesin</label>
                 <div class="input-group col-md-9 col-xs-8">
-                    <select id='searchBuyer' name="searchBuyer" class="js-states form-control" placeholder="- all -"></select>
+                    <select id='searchBuyer' name="searchBuyer" class="form-control" placeholder="- all -">
+                        <option value="">- all -</option>
+                        @foreach ($machine as $item)
+                            <option value="{{ $item->id }}">{{ $item->machinename }}</option>
+                        @endforeach
+                    </select>
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-md-3 col-xs-4" resources="OrgDivision">Status</label>
+                <label class="control-label col-md-3 col-xs-4">Status</label>
                 <div class="input-group col-md-9 col-xs-8">
-                    <select id="printStatus" class="js-states form-control" placeholder="- all -">
+                    <select class="form-control" placeholder="- all -">
                         <option value="">- all -</option>
                         <option value="0">Belum LPK</option>
                         <option value="1">SUdah LPK</option>
@@ -89,7 +79,9 @@
     
         <div class="col-lg-12" style="border-top:1px solid #efefef">
             <div class="toolbar">
-                <button id="btnFilter" type="button" class="btn btn-info" style="width:125px;"><i class="fa fa-search"></i> Filter</button>
+                <button id="btnFilter" wire:click="search" type="button" class="btn btn-info" style="width:125px;">
+                    <i class="fa fa-search"></i> Filter
+                </button>
                 <button 
                     id="btnCreate" 
                     type="button" 
@@ -124,7 +116,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($tdOrderLpk as $item)
+                        @foreach ($seitai as $item)
                         <tr>
                             <td>
                                 <a href="{{ route('edit-seitai', ['orderId' => $item->id]) }}" class="btn btn-info">
@@ -135,34 +127,34 @@
                                 {{ $item->lpk_no }}
                             </td>
                             <td>
-                                {{ $item->lpk_date }}
-                            </td>
-                            <td>
-                                {{ $item->panjang_lpk }}
-                            </td>
-                            <td>
                                 {{ $item->qty_lpk }}
                             </td>
                             <td>
-                                {{ $item->qty_gentan }}
+                                {{ $item->qty_produksi }}
                             </td>
                             <td>
-                                -
+                                
                             </td>
                             <td>
-                                -
+                                {{ $item->order_id }}
                             </td>
                             <td>
-                                -
+                                {{ $item->machine_id }}
                             </td>
                             <td>
-                                -
+                                {{ $item->production_date }}
                             </td>
                             <td>
-                                -
+                                
                             </td>
                             <td>
-                                -
+                                {{ $item->work_hour }}
+                            </td>
+                            <td>
+                                {{ $item->work_shift }}
+                            </td>
+                            <td>
+                                {{ $item->seq_no }}
                             </td>
                         </tr>
                         @endforeach
