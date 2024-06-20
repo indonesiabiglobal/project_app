34d888072176bea433a82cf74a3d6817de8aa0d3
resna <resnapangestu01@gmail.com>
2024-06-21 03:24:50 +0700

    -

--- app/Http/Livewire/LossSeitaiController.php
+++ app/Http/Livewire/LossSeitaiController.php
@@ -2,10 +2,169 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\MsMachine;
+use Carbon\Carbon;
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
 
 class LossSeitaiController extends Component
 {
+    public $loss = [];
+    // public $product;
+    // public $buyer;
+    public $tglMasuk;
+    public $tglKeluar;
+    public $machine = [];
+    public $transaksi;
+    public $machine_id;
+
+    public function mount()
+    {
+        // $this->product = MsProduct::limit(10)->get();
+        // $this->buyer = MsBuyer::limit(10)->get();
+        $this->machine = MsMachine::limit(10)->get();
+        $this->tglMasuk = Carbon::now()->format('Y-m-d');
+        $this->tglKeluar = Carbon::now()->format('Y-m-d'); 
+    }
+
+        // public function updatedStreetAddress()
+        // {
+        //     dd($this->machine_id);
+        //     if($this->streetAddress != '') {
+        //         $this->machine = MsMachine::searchAddress($this->machine_id);
+        //     } else {
+        //         $this->machine = [];
+        //     }
+        // }
+
+    public function search(){
+        if($this->transaksi == 2){
+            // $tglMasuk = '';
+            // if (isset($this->tglMasuk) && $this->tglMasuk != '') {
+            //     $tglMasuk = "WHERE tdpg.production_date >= '" . $this->tglMasuk . "'";
+            // }
+            // $tglKeluar = '';
+            // if (isset($this->tglKeluar) && $this->tglKeluar != '') {
+            //     $tglKeluar = "AND tdpg.production_date <= '" . $this->tglKeluar . "'";
+            // }
+            // // produk
+            // // mesin
+            // // status
+            // $searchTerm = '';
+            // if (isset($this->searchTerm) && $this->searchTerm != '') {
+            //     $searchTerm = "AND (tdol.lpk_no ilike '%" . $this->searchTerm . 
+            //     "%' OR tdpg.production_no ilike '%" . $this->searchTerm . 
+            //     "%' OR tdpg.product_id ilike '%" . $this->searchTerm .
+            //     "%' OR tdpg.machine_id ilike '%" . $this->searchTerm . 
+            //     "%')";
+            // }
+
+            // $this->loss = DB::select("
+            // SELECT
+            //     tdpg.ID AS ID,
+            //     tdpg.production_no AS production_no,
+            //     tdpg.production_date AS production_date,
+            //     tdpg.employee_id AS employee_id,
+            //     tdpg.employee_id_infure AS employee_id_infure,
+            //     tdpg.work_shift AS work_shift,
+            //     tdpg.work_hour AS work_hour,
+            //     tdpg.machine_id AS machine_id,
+            //     tdpg.lpk_id AS lpk_id,
+            //     tdpg.product_id AS product_id,
+            //     tdpg.qty_produksi AS qty_produksi,
+            //     tdpg.seitai_berat_loss AS seitai_berat_loss,
+            //     tdpg.infure_berat_loss AS infure_berat_loss,
+            //     tdpg.nomor_palet AS nomor_palet,
+            //     tdpg.nomor_lot AS nomor_lot,
+            //     tdpg.seq_no AS seq_no,
+            //     tdpg.status_production AS status_production,
+            //     tdpg.status_warehouse AS status_warehouse,
+            //     tdpg.kenpin_qty_loss AS kenpin_qty_loss,
+            //     tdpg.kenpin_qty_loss_proses AS kenpin_qty_loss_proses,
+            //     tdpg.created_by AS created_by,
+            //     tdpg.created_on AS created_on,
+            //     tdpg.updated_by AS updated_by,
+            //     tdpg.updated_on AS updated_on,
+            //     tdol.order_id AS order_id,
+            //     tdol.lpk_no AS lpk_no,
+            //     tdol.lpk_date AS lpk_date,
+            //     tdol.panjang_lpk AS panjang_lpk,
+            //     tdol.qty_gentan AS qty_gentan,
+            //     tdol.qty_gulung AS qty_gulung,
+            //     tdol.qty_lpk AS qty_lpk,
+            //     tdol.total_assembly_qty AS total_assembly_qty 
+            // FROM
+            //     tdProduct_Goods AS tdpg
+            //     INNER JOIN tdOrderLpk AS tdol ON tdpg.lpk_id = tdol.ID 
+            // $tglMasuk
+            // $tglKeluar
+            // $searchTerm
+            // limit 5
+            // ");
+        } else {
+            $tglMasuk = '';
+            if (isset($this->tglMasuk) && $this->tglMasuk != '') {
+                $tglMasuk = "WHERE tdpg.created_on >= '" . $this->tglMasuk . "'";
+            }
+            $tglKeluar = '';
+            if (isset($this->tglKeluar) && $this->tglKeluar != '') {
+                $tglKeluar = "AND tdpg.created_on <= '" . $this->tglKeluar . "'";
+            }
+            // produk
+            // mesin
+            // status
+            $searchTerm = '';
+            if (isset($this->searchTerm) && $this->searchTerm != '') {
+                $searchTerm = "AND (tdol.lpk_no ilike '%" . $this->searchTerm . 
+                "%' OR tdpg.production_no ilike '%" . $this->searchTerm . 
+                "%' OR tdpg.product_id ilike '%" . $this->searchTerm .
+                "%' OR tdpg.machine_id ilike '%" . $this->searchTerm . 
+                "%')";
+            }
+
+            $this->loss = DB::select("
+            SELECT 
+                tdpg.id AS id, 
+                tdpg.production_no AS production_no, 
+                tdpg.production_date AS production_date, 
+                tdpg.employee_id AS employee_id, 
+                tdpg.employee_id_infure AS employee_id_infure, 
+                tdpg.work_shift AS work_shift, 
+                tdpg.work_hour AS work_hour, 
+                tdpg.machine_id AS machine_id, 
+                tdpg.lpk_id AS lpk_id, 
+                tdpg.product_id AS product_id, 
+                tdpg.qty_produksi AS qty_produksi, 
+                tdpg.seitai_berat_loss AS seitai_berat_loss, 
+                tdpg.infure_berat_loss AS infure_berat_loss, 
+                tdpg.nomor_palet AS nomor_palet, 
+                tdpg.nomor_lot AS nomor_lot, 
+                tdpg.seq_no AS seq_no, 
+                tdpg.status_production AS status_production, 
+                tdpg.status_warehouse AS status_warehouse, 
+                tdpg.kenpin_qty_loss AS kenpin_qty_loss, 
+                tdpg.kenpin_qty_loss_proses AS kenpin_qty_loss_proses, 
+                tdpg.created_by AS created_by, 
+                tdpg.created_on AS created_on, 
+                tdpg.updated_by AS updated_by, 
+                tdpg.updated_on AS updated_on, 
+                tdol.order_id AS order_id, 
+                tdol.lpk_no AS lpk_no, 
+                tdol.lpk_date AS lpk_date, 
+                tdol.panjang_lpk AS panjang_lpk, 
+                tdol.qty_gentan AS qty_gentan, 
+                tdol.qty_gulung AS qty_gulung, 
+                tdol.qty_lpk AS qty_lpk, 
+                tdol.total_assembly_qty AS total_assembly_qty
+            FROM tdProduct_Goods AS tdpg
+                INNER JOIN tdOrderLpk AS tdol ON tdpg.lpk_id = tdol.id
+            $tglMasuk
+            $tglKeluar
+            limit 5
+            ");
+        }
+    }
+
     public function render()
     {
         return view('livewire.nippo-seitai.loss-seitai');
--- resources/views/livewire/nippo-seitai/loss-seitai.blade.php
+++ resources/views/livewire/nippo-seitai/loss-seitai.blade.php
@@ -1,5 +1,5 @@
-<title>Loss Seitai</title>
-<div class="container mt-5">
+{{-- <title>Loss Seitai</title> --}}
+<div class="container mt-4">
     <div class="row">
         @if (session()->has('message'))
             <div class="alert alert-success">
@@ -8,39 +8,22 @@
         @endif
         <div class="col-lg-6 mb-4">
             <div class="form-group">
-                <label class="control-label col-md-3 col-xs-4" resources="DatePeriod"><span class="hidden-xs" style="white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">Filter </span>Tanggal</label>
+                <label class="control-label col-md-3 col-xs-4"><span class="hidden-xs" style="white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">Filter </span>Tanggal</label>
                 <div class="input-group col-md-9 col-xs-8">
                     <table>
                         <tr style="white-space:nowrap">
                             <td class="hidden-xs" valign="top">
-                                <select class="form-select mb-0" id="gender"
-                                    aria-label="Gender select example">
-                                    <option selected>Proses</option>
-                                    <option value="Female">Order</option>
+                                <select class="form-select mb-0" wire:model.defer="transaksi">
+                                    <option value="1">Proses</option>
+                                    <option value="2">Order</option>
                                 </select>
                             </td>
                             <td>
                                 <div class="form-group" style="margin-left:1px; white-space:nowrap">
                                     <div class="input-group">
-                                        <span class="input-group-text"><svg class="icon icon-xs" fill="currentColor"
-                                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                                clip-rule="evenodd"></path>
-                                        </svg></span>
-                                    <input data-datepicker=""
-                                        class="form-control datepicker-input" id="birthday" type="text"
-                                        placeholder="yyyy/mm/dd">
+                                        <input class="form-control datepicker-input" type="date" wire:model.defer="tglMasuk" placeholder="yyyy/mm/dd"/>
 
-                                        <span class="input-group-text"><svg class="icon icon-xs" fill="currentColor"
-                                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                                clip-rule="evenodd"></path>
-                                        </svg></span>
-                                    <input data-datepicker=""
-                                        class="form-control datepicker-input" id="birthday" type="text"
-                                        placeholder="yyyy/mm/dd">
+                                        <input class="form-control datepicker-input" type="date" wire:model.defer="tglKeluar" placeholder="yyyy/mm/dd"/>
                                     </div>
                                 </div>
                             </td>
@@ -48,37 +31,52 @@
                     </table>
                 </div>
             </div>
-            <div class="form-group">
+            {{-- <div class="form-group">
                 <label class="control-label col-md-3 col-xs-4" resources="Search">Nomor LPK </label>
                 <div class="input-group col-md-9 col-xs-8">
                     <input id='searchText' name='searchText' class="form-control" type="text" resources-placeholder="SearchTextOrCode" placeholder="search nomor PO, nama produk" />
                 </div>
-            </div>
+            </div> --}}
             <div class="form-group">
                 <label class="control-label col-md-3 col-xs-4" resources="Search">Search</label>
                 <div class="input-group col-md-9 col-xs-8">
-                    <input id='searchText' name='searchText' class="form-control" type="text" resources-placeholder="SearchTextOrCode" placeholder="search nomor PO, nama produk" />
+                    <input id='searchText' name='searchText' class="form-control" type="text"  placeholder="search nomor PO, nama produk" />
                 </div>
             </div>
         </div>
     
         <div class="col-lg-6">
-            <div class="form-group">
+            {{-- <div class="form-group">
                 <label class="control-label col-md-3 col-xs-4" resources="OrgBranch">Produk</label>
                 <div class="input-group col-md-9 col-xs-8">
                     <select id='searchProd' name="searchProd" class="js-states form-control" placeholder="- all -"></select>
                 </div>
-            </div>
+            </div> --}}
+            {{-- <input type="text" list="machineOptions" wire:model="machine_id" class="input-group col-md-9 col-xs-8" x-on:change.debounce="addressSelected($event)" >
+            <datalist id="machineOptions">
+                @foreach($machine as $result)
+                    <option
+                        wire:key="{{ $result->id }}"
+                        data-value="{{ $result->id }}"
+                        value="{{ $result->machinename }}"
+                    ></option>
+                @endforeach
+            </datalist> --}}
             <div class="form-group">
-                <label class="control-label col-md-3 col-xs-4" resources="OrgDivision">Mesin</label>
+                <label class="control-label col-md-3 col-xs-4">Mesin</label>
                 <div class="input-group col-md-9 col-xs-8">
-                    <select id='searchBuyer' name="searchBuyer" class="js-states form-control" placeholder="- all -"></select>
+                    <select id='searchBuyer' name="searchBuyer" class="form-control" placeholder="- all -">
+                        <option value="">- all -</option>
+                        @foreach ($machine as $item)
+                            <option value="{{ $item->id }}">{{ $item->machinename }}</option>
+                        @endforeach
+                    </select>
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-md-3 col-xs-4" resources="OrgDivision">Status</label>
+                <label class="control-label col-md-3 col-xs-4">Status</label>
                 <div class="input-group col-md-9 col-xs-8">
-                    <select id="printStatus" class="js-states form-control" placeholder="- all -">
+                    <select class="form-control" placeholder="- all -">
                         <option value="">- all -</option>
                         <option value="0">Belum LPK</option>
                         <option value="1">SUdah LPK</option>
@@ -89,7 +87,9 @@
     
         <div class="col-lg-12" style="border-top:1px solid #efefef">
             <div class="toolbar">
-                <button id="btnFilter" type="button" class="btn btn-info" style="width:125px;"><i class="fa fa-search"></i> Filter</button>
+                <button id="btnFilter" wire:click="search" type="button" class="btn btn-info" style="width:125px;">
+                    <i class="fa fa-search"></i> Filter
+                </button>
             </div>
             <table class="table table-bordered" data-height="414" id="tableSrc"></table>
         </div>
@@ -116,55 +116,48 @@
                     </thead>
                     <tbody>
                         <!-- Item -->
+                        @foreach ($loss as $item)
                         <tr>
-                            <td><a href="#" class="text-primary fw-bold">1</a> </td>
-                            <td class="fw-bold d-flex align-items-center">
-                                <svg class="icon icon-xxs text-gray-500 me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clip-rule="evenodd"></path></svg>
-                                Direct
+                            <td>
+                                <a href="{{ route('edit-seitai', ['orderId' => $item->id]) }}" class="btn btn-info">
+                                    <i class="fa fa-edit"></i> Edit
+                                </a>
+                            </td>
+                            <td>                                
+                                {{ $item->lpk_no }}
                             </td>
                             <td>
-                                Direct
+                                {{ $item->qty_lpk }}
                             </td>
                             <td>
-                               - 
+                                {{ $item->qty_produksi }}
                             </td>
                             <td>
-                               --
+                                
                             </td>
                             <td>
-                                <div class="row d-flex align-items-center">
-                                    <div class="col-12 col-xl-2 px-0">
-                                        <div class="small fw-bold">51%</div>
-                                    </div>
-                                    <div class="col-12 col-xl-10 px-0 px-xl-1">
-                                        <div class="progress progress-lg mb-0">
-                                            <div class="progress-bar bg-dark" role="progressbar" aria-valuenow="51" aria-valuemin="0" aria-valuemax="100" style="width: 51%;"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-success">
-                                <div class="d-flex align-items-center">
-                                    <svg class="icon icon-xs me-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>                                   
-                                    <span class="fw-bold">2.45%</span>
-                                </div>
+                                {{ $item->order_id }}
                             </td>
                             <td>
-                                Direct
+                                {{ $item->machine_id }}
                             </td>
                             <td>
-                               - 
+                                {{ $item->production_date }}
                             </td>
                             <td>
-                               --
+                                
                             </td>
                             <td>
-                                - 
-                             </td>
-                             <td>
-                                --
-                             </td>
+                                {{ $item->work_hour }}
+                            </td>
+                            <td>
+                                {{ $item->work_shift }}
+                            </td>
+                            <td>
+                                {{ $item->seq_no }}
+                            </td>
                         </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
