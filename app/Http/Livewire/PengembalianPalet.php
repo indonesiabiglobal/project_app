2b3bdeb1df7ab686cb677f0cdec713ca87a57b4d
resna <resnapangestu01@gmail.com>
2024-06-21 10:43:42 +0700

    -

--- app/Http/Livewire/PengembalianPalet.php
+++ app/Http/Livewire/PengembalianPalet.php
@@ -2,10 +2,57 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
 
 class PengembalianPalet extends Component
 {
+    public $data = [];
+    public $tglMasuk;
+    public $tglKeluar;
+    public $machine;
+    public $transaksi;
+    public $searchTerm;
+
+    public function mount()
+    {
+        $this->tglMasuk = Carbon::now()->format('Y-m-d');
+        $this->tglKeluar = Carbon::now()->format('Y-m-d'); 
+    }
+
+    public function search(){
+        $tglMasuk = '';
+        if (isset($this->tglMasuk) && $this->tglMasuk != '') {
+            $tglMasuk = "WHERE tdpg.production_date >= '" . $this->tglMasuk . "'";
+        }
+        $tglKeluar = '';
+        if (isset($this->tglKeluar) && $this->tglKeluar != '') {
+            $tglKeluar = "AND tdpg.production_date <= '" . $this->tglKeluar . "'";
+        }
+        $searchTerm = '';
+        if (isset($this->searchTerm) && $this->searchTerm != '') {
+            $searchTerm = " WHERE tdpg.nomor_palet ilike '%" . $this->searchTerm .
+            "%'";
+        }
+
+        $this->data = DB::select("
+        SELECT
+            X.product_id AS product_id,
+            X.nomor_palet AS nomor_palet 
+        FROM
+            (
+            SELECT DISTINCT
+                tdpg.product_id AS product_id,
+                tdpg.nomor_palet AS nomor_palet 
+            FROM
+                tdProduct_Goods AS tdpg
+                $searchTerm                 
+            ) AS X
+        limit 5
+        ");
+    }
+
     public function render()
     {
         return view('livewire.warehouse.pengembalian-palet');
--- resources/views/livewire/warehouse/pengembalian-palet.blade.php
+++ resources/views/livewire/warehouse/pengembalian-palet.blade.php
@@ -1,11 +1,11 @@
-<title>Order Entry</title>
-<div class="container mt-5">
+{{-- <title>Order Entry</title> --}}
+<div class="container mt-3">
     <div class="row">
         <div class="col-lg-6 mb-3">
             <div class="form-group">
-                <label class="control-label col-md-3 col-xs-4" resources="Search">Nomor Palet </label>
+                <label class="control-label col-md-3 col-xs-4">Search </label>
                 <div class="input-group col-md-9 col-xs-8">
-                    <input id='searchText' name='searchText' class="form-control" type="text" resources-placeholder="SearchTextOrCode" placeholder="search nomor PO, nama produk" />
+                    <input id='search' name='search' wire:model.defer="searchTerm" class="form-control" type="text" placeholder="search nomor PO, nama produk" />
                 </div>
             </div>
         </div>    
@@ -20,12 +20,10 @@
     
         <div class="col-lg-12" style="border-top:1px solid #efefef">
             <div class="toolbar">
-                <button id="btnFilter" type="button" class="btn btn-info" style="width:125px;"><i class="fa fa-search"></i> Filter</button>
-                <button id="btnCreate" type="button" class="btn btn-success" style="width:125px;" asp-app-role="write">
-                    <i class="fa fa-plus"></i> Add
+                <button id="btnFilter" wire:click="search" type="button" class="btn btn-info" style="width:125px;">
+                    <i class="fa fa-search"></i> Filter
                 </button>
             </div>
-            <table class="table table-bordered" data-height="414" id="tableSrc"></table>
         </div>
     </div>
     <div class="card border-0 shadow mb-4 mt-4">
@@ -42,19 +40,24 @@
                     </thead>
                     <tbody>
                         <!-- Item -->
+                        @foreach ($data as $item)
                         <tr>
-                            <td><a href="#" class="text-primary fw-bold">1</a> </td>
-                            <td class="fw-bold d-flex align-items-center">
-                                <svg class="icon icon-xxs text-gray-500 me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clip-rule="evenodd"></path></svg>
-                                Direct
+                            <td>
+                                {{-- <a href="{{ route('edit-nippo', ['orderId' => $item->id]) }}" class="btn btn-info">
+                                    <i class="fa fa-edit"></i> Edit
+                                </a> --}}
+                            </td>
+                            <td>                                
+                                {{ $item->nomor_palet }}
                             </td>
                             <td>
-                                Direct
+                                
                             </td>
                             <td>
-                               - 
-                            </td>                           
+                                {{ $item->product_id }}
+                            </td>
                         </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
