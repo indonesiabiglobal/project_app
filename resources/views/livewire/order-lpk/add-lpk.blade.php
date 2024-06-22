883bd8cd397b0ec1f8de942f10bbe447a174731b
resna <resnapangestu01@gmail.com>
2024-06-23 00:39:06 +0700

    -

--- resources/views/livewire/order-lpk/add-lpk.blade.php
+++ resources/views/livewire/order-lpk/add-lpk.blade.php
@@ -19,15 +19,7 @@
                 <div class="form-group">
                     <label class="control-label col-md-6 col-xs-12">Tanggal LPK</label>
                     <div class="input-group col-md-9 col-xs-12">
-                        <input data-datepicker="" class="form-control datepicker-input readonly" readonly="readonly" id="lpk_date" type="text" placeholder="yyyy/mm/dd" wire:model="lpk_date">
-                        <span class="input-group-text">
-                            <svg class="icon icon-xs" fill="currentColor"
-                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                clip-rule="evenodd"></path>
-                            </svg>
-                        </span>
+                        <input class="form-control datepicker-input" type="date" wire:model.defer="lpk_date" placeholder="yyyy/mm/dd"/>
                     </div>
                 </div>
                 <div class="form-group">
@@ -39,13 +31,13 @@
                 <div class="form-group">
                     <label class="control-label col-md-6 col-xs-12">PO Number</label>
                     <div class="input-group col-md-9 col-xs-12">
-                        <input type="text" id="prev_lpk_no" class="form-control readonly" readonly="readonly" wire:model="prev_lpk_no" />
+                        <input type="text" class="form-control" wire:model="po_no" placeholder="PO NUMBER" />
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="control-label col-md-6 col-xs-12">Nomor Order</label>
                     <div class="input-group col-md-9 col-xs-12">
-                        <input type="text" id="order_id" class="form-control readonly" readonly="readonly"  wire:model="order_id" />
+                        <input type="text" class="form-control readonly" readonly="readonly"  wire:model="no_order" />
                     </div>
                 </div>
                 <div class="form-group">
