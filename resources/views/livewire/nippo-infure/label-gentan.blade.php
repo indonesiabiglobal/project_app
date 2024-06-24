4a69d16898fac5b768f19393682315a4c343d549
resna <resnapangestu01@gmail.com>
2024-06-25 01:18:37 +0700

    -

--- resources/views/livewire/nippo-infure/label-gentan.blade.php
+++ resources/views/livewire/nippo-infure/label-gentan.blade.php
@@ -2,67 +2,74 @@
 	<div class="col-lg-2"></div>
 	<div class="col-lg-8">
 		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12">Nomor LPK</label>
-			<div class="input-group col-md-9 col-xs-12">
-				<input type="text" id="lpk_no" class="form-control lpkManual" tabindex="1" />
+			<div class="input-group">
+				<label class="control-label col-12 col-lg-3">Nomor LPK</label>
+				<input type="text" id="lpk_no" class="form-control" placeholder="000000-000"/>
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12">Nomor Gentan</label>
-			<div class="input-group col-md-9 col-xs-12">
-				<input type="text" id="gentan_no" class="form-control gentan_no integer" maxlength="3" tabindex="2" />
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<label class="control-label col-12 col-lg-3">Nomor Gentan</label>
+				<input type="text" id="gentan_no" class="form-control" placeholder="..." />
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12">Nomor Order</label>
-			<div class="input-group col-md-9 col-xs-12">
-				<input type="text" name="product_code" class="form-control readonly" readonly="readonly" tabindex="-1" />
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<label class="control-label col-12 col-lg-3">Nomor Order</label>
+				<input type="text" name="product_code" class="form-control readonly" readonly="readonly" />
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12">Nama Produk</label>
-			<div class="input-group col-md-9 col-xs-12">
-				<input type="text" name="product_name" class="form-control readonly" readonly="readonly" tabindex="-1" />
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<label class="control-label col-12 col-lg-3">Nama Produk</label>
+				<input type="text" name="product_name" class="form-control readonly" readonly="readonly" />
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12">Panjang Produksi</label>
-			<div class="input-group col-md-9 col-xs-12">
-				<input type="text" name="panjang_produksi" class="form-control readonly currency" readonly="readonly" tabindex="-1" />
-				<div class="input-group-addon">meter</div>
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<label class="control-label col-12 col-lg-3">Panjang Produksi</label>
+				<input type="text" name="panjang_produksi" class="form-control readonly currency" readonly="readonly" />
+				<span class="input-group-text">
+					meter
+				</span>
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12">Berat Gentan</label>
-			<div class="input-group col-md-9 col-xs-12">
-				<input type="text" name="berat_produksi" class="form-control readonly currency" readonly="readonly" tabindex="-1" />
-				<div class="input-group-addon">kg</div>
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<label class="control-label col-12 col-lg-3">Berat Gentan</label>
+				<input type="text" name="berat_produksi" class="form-control readonly currency" readonly="readonly" />
+				<span class="input-group-text">
+					kg
+				</span>
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12">Berat Standard</label>
-			<div class="input-group col-md-9 col-xs-12">
-				<input type="text" name="berat_standard" class="form-control readonly currency" readonly="readonly" tabindex="-1" />
-				<div class="input-group-addon">kg</div>
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<label class="control-label col-12 col-lg-3">Berat Standard</label>
+				<input type="text" name="berat_standard" class="form-control readonly currency" readonly="readonly" />
+				<span class="input-group-text">
+					kg
+				</span>
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12">Tanggal LPK</label>
-			<div class="input-group col-md-9 col-xs-12">
-				<input type="text" name="lpk_date" class="form-control readonly" readonly="readonly" tabindex="-1" />
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<label class="control-label col-12 col-lg-3">Tanggal LPK</label>
+				<input class="form-control readonly datepicker-input" readonly="readonly" type="date" wire:model.defer="tglMasuk" />
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12">Jumlah LPK</label>
-			<div class="input-group col-md-9 col-xs-12">
-				<input type="text" name="qty_lpk" class="form-control readonly integer" readonly="readonly" tabindex="-1" />
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<label class="control-label col-12 col-lg-3">Jumlah LPK</label>
+				<input type="text" name="qty_lpk" class="form-control readonly integer" readonly="readonly" />
 			</div>
 		</div>
 		<hr />
 		<div class="form-group">
-			<label class="control-label col-md-3 col-xs-12"></label>
-			<div class="input-group col-md-9 col-xs-12">
-				<button type="button" class="btn btn-success btn-print" disabled="disabled"><i class="fa fa-print"></i> Print</button>
+			<div class="input-group">				
+				<button type="button" class="btn btn-success btn-print" disabled="disabled">
+					<i class="fa fa-print"></i> Print
+				</button>
 				<div style="float:right" class="text-danger">Paper: A4-Portrait or Thermal </div>
 			</div>
 		</div>
