f6e10e5d5327028ea82954d9b9a5e5d7962a008f
resna <resnapangestu01@gmail.com>
2024-06-25 19:23:53 +0700

    -

--- resources/views/livewire/nippo-seitai/check-list-seitai.blade.php
+++ resources/views/livewire/nippo-seitai/check-list-seitai.blade.php
@@ -2,92 +2,102 @@
 	<div class="col-lg-2"></div>
 	<div class="col-lg-6">
 		<div class="form-group">
-			<label class="control-label col-md-4 col-xs-12">Tanggal Produksi</label>
-			<div class="input-group col-md-8 col-xs-12">
+			<div class="input-group col-md-9 col-xs-8">
+				<label class="control-label col-4">Tanggal Produksi</label>
+				<div class="col-12 col-lg-8">
+					<select class="form-select mb-0" wire:model.defer="transaksi">
+						<option value="1">Produksi</option>
+						<option value="2">Order</option>
+					</select>
+				</div>
+			</div>
+			<div class="col-12 mt-1">
 				<div class="form-group">
 					<div class="input-group">
-						<span class="input-group-addon" style="padding:2px;width:55px!important">Awal:&nbsp;</span>
-						<input  data-datepicker="" id='searchDate1' class="form-control datepicker-input" type="text" placeholder="yyyy/mm/dd"  formatter='date, dd MMM yyyy' required="required" />
-                        <span class="input-group-text"><svg class="icon icon-xs" fill="currentColor"
-                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                clip-rule="evenodd"></path></svg>
-                        </span>
+						<span class="input-group-addon col-12 col-lg-2">Awal: </span>
+						<input class="form-control datepicker-input" type="datetime-local" wire:model.defer="tglMasuk" placeholder="yyyy/mm/dd hh:mm" />
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-4 col-xs-12"></label>
-			<div class="input-group col-md-8 col-xs-12">
+		<div class="form-group mt-1">
+			<div class="col-12">
 				<div class="form-group">
 					<div class="input-group">
-						<span class="input-group-addon" style="padding:2px;width:55px!important">Akhir:&nbsp;</span>
-						<input  data-datepicker="" id='searchDate1' class="form-control datepicker-input" type="text" placeholder="yyyy/mm/dd"  formatter='date, dd MMM yyyy' required="required" />
-                        <span class="input-group-text"><svg class="icon icon-xs" fill="currentColor"
-                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                clip-rule="evenodd"></path></svg>
-                        </span>
+						<span class="input-group-addon col-12 col-lg-2">Akhir: </span>
+						<input class="form-control datepicker-input" type="datetime-local" wire:model.defer="tglAkhir" placeholder="yyyy/mm/dd hh:mm" />
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-4 col-xs-12">Jenis Report</label>
-			<div class="input-group col-md-8 col-xs-12">
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<span class="input-group-addon col-12 col-lg-3">Jenis Report </span>
 				<select id="department" class="form-control" placeholder="- pilih jenis report -" onchange="changeDep(this.value)">
 					<option value="1">Check List</option>
 					<option value="2">Loss Infure</option>
 				</select>
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-4 col-xs-12">Nomor LPK</label>
-			<div class="input-group col-md-8 col-xs-12">
-				<div class="form-group">
-					<input id="lpk_no" type="text" class="form-control lpkManual" placeholder=".." tabindex="1">
-				</div>
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<span class="input-group-addon col-12 col-lg-3">Nomor Proses </span>
+				<input type="text" class="form-control" placeholder="1">
+				<span class="input-group-text readonly" readonly="readonly">
+					~
+				</span>
+				<input type="text" class="form-control" placeholder="1000">
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-4 col-xs-12">Nomor Order</label>
-			<div class="input-group col-md-8 col-xs-12">
-				<input id="product_code" type="text" class="form-control" placeholder=".." tabindex="2">
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<span class="input-group-addon col-12 col-lg-3">Nomor LPK </span>
+				<input type="text" class="form-control" placeholder="000000-000">
 			</div>
 		</div>
-        <div class="form-group">
-			<label class="control-label col-md-4 col-xs-12">Jenis Report</label>
-			<div class="input-group col-md-8 col-xs-12">
-				<select id="department" class="form-control" placeholder="- pilih jenis report -" onchange="changeDep(this.value)">
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<span class="input-group-addon col-12 col-lg-3">Nomor Order </span>
+				<input type="text" class="form-control" placeholder="..">
+			</div>
+		</div>
+        <div class="form-group mt-1">
+			<div class="input-group">
+				<span class="input-group-addon col-12 col-lg-3">Departement</span>
+				<select class="form-control">
+					<option value="">- all -</option>
 					<option value="1"></option>
 					<option value="2"></option>
 				</select>
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-4 col-xs-12" resources="OrgDivision">Mesin</label>
-			<div class="input-group col-md-8 col-xs-12">
-				<select id='searchMachine' class="js-states form-control" placeholder="- all -" tabindex="3" onchange="$('#searchShift').focus();"></select>
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<span class="input-group-addon col-12 col-lg-3">Mesin</span>
+				<select class="form-control">
+					<option value="">- all -</option>
+					<option value="1"></option>
+					<option value="2"></option>
+				</select>
 			</div>
 		</div>
-		<div class="form-group">
-			<label class="control-label col-md-4 col-xs-12">Nomor Han</label>
-			<div class="input-group col-md-8 col-xs-12">
-				<div class="form-group">
-					<input id="searchNoHand" type="text" class="form-control nomor_han" placeholder=".." tabindex="4" style = "text-transform: uppercase" />
-				</div>
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<span class="input-group-addon col-12 col-lg-3">Nomor Palet</span>
+				<input type="text" class="form-control" placeholder="A0000-000000" />
 			</div>
 		</div>
-		<br />
-
+		<div class="form-group mt-1">
+			<div class="input-group">
+				<span class="input-group-addon col-12 col-lg-3">Nomor LOT</span>
+				<input type="text" class="form-control" placeholder="---" />
+			</div>
+		</div>
 		<hr />
 		<div class="form-group">
 			<label class="control-label col-md-4 col-xs-12"></label>
-			<div class="input-group col-md-8 col-xs-12">
-				<button type="button" class="btn btn-success btn-print" style="width:99%" tabindex="8"><i class="fa fa-print"></i> Generate Report</button>
+			<div class="input-group">
+				<button type="button" class="btn btn-success btn-print" style="width:99%"><i class="fa fa-print"></i> Generate Report</button>
 			</div>
 		</div>
 	</div>
