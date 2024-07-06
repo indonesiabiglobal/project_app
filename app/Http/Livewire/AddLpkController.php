1c391368373dc11eb1f23a375e0693fc75cf556e
resna <resnapangestu01@gmail.com>
2024-07-06 14:42:20 +0700

    -

--- app/Http/Livewire/AddLpkController.php
+++ app/Http/Livewire/AddLpkController.php
@@ -156,12 +156,15 @@ class AddLpkController extends Component
                 $this->machinename = $machine->machinename;
             }
         }
-
-        $this->total_assembly_line = $this->qty_lpk * $this->productlength;
-        $this->qty_gentan = $this->productlength / $this->defaultgulung;
-        $this->qty_gulung = $this->productlength * $this->qty_gentan;
-        $this->panjang_lpk = $this->qty_gentan * $this->qty_gulung;
-        $this->selisihkurang = $this->productlength - $this->panjang_lpk;
+        if(isset($this->qty_lpk) && isset($this->productlength)){
+            $this->total_assembly_line = $this->qty_lpk * $this->productlength;
+            $this->qty_gentan = $this->productlength / $this->defaultgulung;
+            $this->qty_gulung = $this->productlength * $this->qty_gentan;
+            $this->panjang_lpk = $this->qty_gentan * $this->qty_gulung;
+            $this->selisihkurang = $this->productlength - $this->panjang_lpk;
+        }
+        
+        
 
         return view('livewire.order-lpk.add-lpk');
     }
